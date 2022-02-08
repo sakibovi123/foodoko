d5059016fc7d8759be4f3b2efbd339bee65d7df2
sakibovi123 <sakibovi321321@gmail.com>
2022-02-08 22:20:45 +0600

    8th Feb

---
+++ static/js/index.js
@@ -0,0 +1,39 @@
+
+$('.banner______part').slick({
+    dots: false,
+    infinite: true,
+    speed: 200,
+    autoplay: true,
+    fade: true,
+    arrows:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
